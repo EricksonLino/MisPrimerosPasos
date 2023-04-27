824087ebd7edf4a2a473804c23704fe5d373f05d
Erickson L <linobarrantes@gmail.com>
2023-04-26 23:38:56 -0500

    instalamos ef sql server y creamos el applicationDbcontext

---
+++ MisPrimerosPasos.Repository/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MisPrimerosPasos.Entity;
+
+namespace MisPrimerosPasos.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Alumno> Alumnos { get; set; }
+
+    }
+}
