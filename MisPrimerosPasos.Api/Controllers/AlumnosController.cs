1350181bb628d2c0549f88996358985ba2fb35e5
Erickson L <linobarrantes@gmail.com>
2023-04-27 01:29:51 -0500

    creamos alumnos controller listar alumnos e inyectamos application y repository en el program

---
+++ MisPrimerosPasos.Api/Controllers/AlumnosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MisPrimerosPasos.Application.Interfaces;
+using MisPrimerosPasos.Entity;
+
+namespace MisPrimerosPasos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnosController : ControllerBase
+    {
+        private readonly IAlumnoApplication alumnoApplication;
+
+        public AlumnosController(IAlumnoApplication alumnoApplication)
+        {
+            this.alumnoApplication = alumnoApplication;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Alumno>>> ListarAlumnos() 
+        {
+            var alumnos = await this.alumnoApplication.ListarAlumnos();
+            return alumnos;
+        }
+    }
+}
--- MisPrimerosPasos.Api/Program.cs
+++ MisPrimerosPasos.Api/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using MisPrimerosPasos.Application.Implementaciones;
+using MisPrimerosPasos.Application.Interfaces;
 using MisPrimerosPasos.Repository;
+using MisPrimerosPasos.Repository.Implementaciones;
+using MisPrimerosPasos.Repository.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(cfg => 
                                     cfg.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IAlumnoRepository, AlumnoRepository>();
+builder.Services.AddScoped<IAlumnoApplication, AlumnoApplication>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
